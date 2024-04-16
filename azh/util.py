dd6aa41d9779226a5a581d58c75dfd785306a2c5
Yannick Fischer <yannick.fischer@cern.ch>
2024-04-16 17:32:08 +0200

    Added util function

---
+++ azh/util.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from columnflow.util import maybe_import
+
+ak = maybe_import("awkward")
+
+
+def masked_sorted_indices(mask: ak.Array, sort_var: ak.Array, ascending: bool = False) -> ak.Array:
+    """
+    Helper function to obtain the correct indices of an object mask
+    """
+    indices = ak.argsort(sort_var, axis=-1, ascending=ascending)
+    return indices[mask[indices]]
